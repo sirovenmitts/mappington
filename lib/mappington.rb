9e89d078fd481db8032038abb0b1bb2ad4c1f38f
Kenneth Pullen <kenneth.pullen@gmail.com>
2012-09-01 08:27:15 -0700

    Add way to quickly generate URI without messing with Map
    Mappington#uri_for takes a place (an address or coordinate) and returns a
    URI pointing to a map centered on the place. Additionally, a marker is
    dropped there.

--- lib/mappington.rb
+++ lib/mappington.rb
@@ -12,4 +12,15 @@ require 'mappington/map'
 require 'mappington/path_error'
 require 'mappington/map_error'
 
-module Mappington; end
+module Mappington
+	# Quickly generate a URI for an address or coordinate. The map uses
+	# default values for everything, and will have a marker centered on
+	# +place+.
+	def self.uri_for place
+		Map.new(
+			:size => [320, 240],
+			:center_on => place,
+			:zoom_level => 15,
+			:markers => [Marker.new(:at => place)]).to_s
+	end
+end
